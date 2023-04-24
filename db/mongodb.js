1f88b5d6b9a3a062e579a8587d8f93684197f768
Janice Chen <janicechen31997@hotmail.com>
2023-04-24 11:10:11 -0500

    commit

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const url = `mongodb+srv://${process.env.USER_MONGO}:${process.env.DB_PASSWORD}@cluster0.${process.env.MONGO_ID_PASS}.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://janice97:${process.env.DB_PASSWORD}@cluster0.tyylkcs.mongodb.net/?retryWrites=true&w=majority`
 console.log("------>",url)
 
 mongoose.connect(url)
